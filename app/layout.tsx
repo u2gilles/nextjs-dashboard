12672faa847c4f40be4106ea6ba461de079183f1
u2gilles <u2gilles@gmail.com>
2025-01-06 20:40:03 +0700

    chap3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,21 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
+
+/*
+
+
 export default function RootLayout({
   children,
 }: {
@@ -9,3 +27,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+*/
