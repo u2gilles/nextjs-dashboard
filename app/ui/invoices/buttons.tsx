24c9e7a427875e29e9c8bdc1131de1a0b5de4e46
u2gilles <u2gilles@gmail.com>
2025-01-08 14:23:15 +0700

    DeleteInvoice action error in production mode

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -25,12 +25,27 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
-  
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+// export function DeleteInvoice({ id }: { id: string }) {
+//   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+//   return (
+//     <form action={deleteInvoiceWithId}>
+//       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+//         <span className="sr-only">Delete</span>
+//         <TrashIcon className="w-4" />
+//       </button>
+//     </form>
+//   );
+// }
 
+
+export function DeleteInvoice({ id }: { id: string }) {
   return (
-    <form action={deleteInvoiceWithId}>
+    <form
+      action={async (formData: FormData) => {
+        "use server";
+        await deleteInvoice(id);
+      }}
+    >
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-4" />
