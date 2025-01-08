b11cffb8a0bf80be25ccf19535654402c8f1800c
u2gilles <u2gilles@gmail.com>
2025-01-08 15:15:01 +0700

    chap14 create form (to redo)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,99 +6,121 @@ import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
-
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    });
-   
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-   
-    try {
-      await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-      `;
-    } catch (error) {
-      return {
-        message: 'Database Error: Failed to Create Invoice.',
-      };
-    }
-   
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
   }
 
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
 
-  export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    });
-   
-    const amountInCents = amount * 100;
-   
-    try {
-      await sql`
+  // Insert data into the database
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    // If a database error occurs, return a more specific error.
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
+
+  // Revalidate the cache for the invoices page and redirect the user.
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
           UPDATE invoices
           SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
           WHERE id = ${id}
         `;
-    } catch (error) {
-      return { message: 'Database Error: Failed to Update Invoice.' };
-    }
-   
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  } catch (error) {
+    return { message: "Database Error: Failed to Update Invoice." };
   }
 
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
 
 //  Since this action is being called in the /dashboard/invoices path,
 //  you don't need to call redirect. Calling revalidatePath will
 //  trigger a new server request and re-render the table.
 
 export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice');
 
-    // throw new Error('Failed to Delete Invoice');
-
-    try {
-      await sql`DELETE FROM invoices WHERE id = ${id}`;
-      revalidatePath('/dashboard/invoices');
-      return { message: 'Deleted Invoice.' };
-    } catch (error) {
-      return { message: 'Database Error: Failed to Delete Invoice.' };
-    }
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+    return { message: "Deleted Invoice." };
+  } catch (error) {
+    return { message: "Database Error: Failed to Delete Invoice." };
   }
+}
 
+/**
+ * Note how redirect is being called outside of the try/catch block. This is because
+ * redirect works by throwing an error, which would be caught by the catch block.
+ * To avoid this, you can call redirect after try/catch. redirect would only be
+ * reachable if try is successful.
+ *
+ */
 
-  /**
-   * Note how redirect is being called outside of the try/catch block. This is because 
-   * redirect works by throwing an error, which would be caught by the catch block. 
-   * To avoid this, you can call redirect after try/catch. redirect would only be 
-   * reachable if try is successful.
-   * 
-   */
-
-
-  /**
-   * 
-   * Handling 404 errors with the notFound function:
-   * visit : http://localhost:3000/dashboard/invoices/2e94d1ed-d220-449f-9f11-f0bbceed9645/edit
-   * 
-   * 
-   */
+/**
+ *
+ * Handling 404 errors with the notFound function:
+ * visit : http://localhost:3000/dashboard/invoices/2e94d1ed-d220-449f-9f11-f0bbceed9645/edit
+ *
+ *
+ */
--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -25,27 +25,10 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-// export function DeleteInvoice({ id }: { id: string }) {
-//   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
-//   return (
-//     <form action={deleteInvoiceWithId}>
-//       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
-//         <span className="sr-only">Delete</span>
-//         <TrashIcon className="w-4" />
-//       </button>
-//     </form>
-//   );
-// }
-
-
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
-    <form
-      action={async (formData: FormData) => {
-        "use server";
-        await deleteInvoice(id);
-      }}
-    >
+    <form action={deleteInvoiceWithId}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-4" />
@@ -53,3 +36,20 @@ export function DeleteInvoice({ id }: { id: string }) {
     </form>
   );
 }
+
+
+// export function DeleteInvoice({ id }: { id: string }) {
+//   return (
+//     <form
+//       action={async (formData: FormData) => {
+//         "use server";
+//         await deleteInvoice(id);
+//       }}
+//     >
+//       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+//         <span className="sr-only">Delete</span>
+//         <TrashIcon className="w-4" />
+//       </button>
+//     </form>
+//   );
+// }
